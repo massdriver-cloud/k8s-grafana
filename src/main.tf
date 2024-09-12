50b9e7cfa9ea82ed191cc77614a9925c5dd3bde9
mclacore <mclacore@gmail.com>
2024-09-12 10:56:57 -0700

    pinning app module version, fixing outdated k8s app updates

--- src/main.tf
+++ src/main.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "application" {
-  source  = "github.com/massdriver-cloud/terraform-modules//massdriver-application"
+  source  = "github.com/massdriver-cloud/terraform-modules//massdriver-application?ref=e1a33e9"
   name    = var.md_metadata.name_prefix
   service = "kubernetes"
 
